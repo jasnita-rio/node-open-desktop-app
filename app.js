8d6096541a07c792a5287454b8275ed060c5f300
Rio Wibowo <satrio.wibowo@jasnita.co.id>
2024-01-11 14:59:08 +0700

    alt + tab

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000
 
 app.get('/open-app', (req, res) => {
   // Extract the parameter from the query string, defaulting to 'notepad' if not provided
-  const nik = req.query.nik || '123456';
+  const nik = req.query.nik || null;
 
   // Run openApp.js script with the provided parameter
   const command = `node openApp.js ${nik}`;
@@ -17,11 +17,19 @@ app.get('/open-app', (req, res) => {
       res.status(500).send('Internal Server Error');
       return;
     }
-    console.log(`Script executed successfully`);
+    console.log('Script executed successfully');
     res.send('Script executed successfully');
+    // res.send(`
+    // <script>
+    //   const newTab = window.open('', '_blank');
+    //   newTab.document.write('<html><head><title>Closing Tab</title></head><body><p>Opening frista...</p></body></html>');
+    //   newTab.setTimeout(() => { newTab.close(); }, 2500);
+    //   window.close();
+    // </script>
+    // `);  
   });
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on http://localhost:${port}`)
 })
--- openApp.js
+++ openApp.js
@@ -1,25 +1,35 @@
 const { keyboard, Key, clipboard } = require("@nut-tree/nut-js");
 
 (async () => {
-    // ALT + TAB
-    // await keyboard.pressKey(Key.LeftAlt, Key.Tab);
-    // await keyboard.releaseKey(Key.LeftAlt, Key.Tab);
-
-    await keyboard.pressKey(Key.LeftSuper);
-    await keyboard.releaseKey(Key.LeftSuper);
-
-    await keyboard.type('notepad');
-
-    await keyboard.pressKey(Key.Enter);
-    await keyboard.releaseKey(Key.Enter);
-
     // Extract the application name from the command line arguments
-    const nik = process.argv[2] || '123456';
-    // await keyboard.type(nik);
+    const nik = process.argv[2];
 
+    // Add NIK to clipboard 
     await clipboard.setContent(nik);
+    
+    // ALT + TAB
+    await keyboard.pressKey(Key.LeftAlt, Key.Tab);
+    await keyboard.releaseKey(Key.LeftAlt, Key.Tab);
+
+    // Open notepad app from windoows search
+
+    // Press windows button
+    // await keyboard.pressKey(Key.LeftSuper);
+    // await keyboard.releaseKey(Key.LeftSuper);
+
+    // Type "notepad"
+    // await keyboard.type('notepad');
+
+    // Enter to open notepad 
+    // await keyboard.pressKey(Key.Enter);
+    // await keyboard.releaseKey(Key.Enter);
+
+    // Type NIK
+    // await keyboard.type(nik);
 
     // Minimize the window using robotjs
     // await keyboard.pressKey(Key.LeftSuper, Key.Down);
     // await keyboard.releaseKey(Key.LeftSuper, Key.Down);
+
+    
 })();
