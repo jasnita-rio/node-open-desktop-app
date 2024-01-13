263e32b6e68e2716ce67e676643290b717ea7c50
Rio Wibowo <satrio.wibowo@jasnita.co.id>
2024-01-13 14:55:39 +0700

    comment on nodeService js

--- nodeService.js
+++ nodeService.js
@@ -1,20 +1,19 @@
 const service = require('node-windows').Service;
 
 const svc = new service({
-    name: "nodeOpenFrista",
+    name: "nodeFristaOpener",
     description: "This script is to run node service to open Frista app",
     script: "C:\\laragon\\www\\node-frista\\app.js"
 })
 
+// Install node windows
 // svc.on('install', function() {
 //     svc.start()
 // })
-
 // svc.install()
 
-
-svc.on('uninstall', function() {
-    console.log('Service unistalled')
-})
-
-svc.uninstall()
+// Uninstall node windows
+// svc.on('uninstall', function() {
+//     console.log('Service unistalled')
+// })
+// svc.uninstall()
